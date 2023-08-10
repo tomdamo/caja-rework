b07abdfffdff69244cad6bc27b445ade720cc1e2
Tom <80176908+tomdamo@users.noreply.github.com>
2023-08-10 12:31:40 +0200

    Initial commit

---
+++ theme/src/config/nav.js
@@ -0,0 +1,14 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'Portfolio',
+		slug: '/blog',
+	},
+	{
+		title: 'Over ons',
+		slug: '/company/about',
+	},
+];
---
+++ theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Lugten Enterprise`,
+	description: `Lugten enterprise is een bedrijf dat zich bezig houdt met zonneweringen, rolluiken, horren en raamdecoratie.`,
+	url: `https://lugtenenterprise.nl`, // No trailing slash!
+	name: `Lugten`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
